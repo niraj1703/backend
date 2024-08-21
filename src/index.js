91b3c95d718a9305b2a1f21a7a9ea60cf7528436
Dhiraj kumar <dhiraj@techfriar.com>
2024-08-21 15:13:58 +0530

    connect mongodb

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
 import express from 'express'
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+// require('dotenv').config({path: './env'})
+// import mongoose from 'mongoose'
+// import app from './app.js'
 const app = express();
+
+
 
 app.get('/',(req,res)=>{
     res.send('server is ready');
 });
 
-const port = process.env.PORT || 3000;
 
 
 
-app.listen(port,()=>{
-    console.log(`serve at http://localhost:${port}`);
-});
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+// app.get('/',(req,res)=>{
+//     res.send('server is ready');
+// });
+
+// const port = process.env.PORT || 3000;
+
+
+
+// app.listen(port,()=>{
+//     console.log(`serve at http://localhost:${port}`);
+// });
